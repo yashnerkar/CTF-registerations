9a6dbf3b2528d09aad669202b845f163a2375f59
yash-png <65919365+yash-png@users.noreply.github.com>
2021-12-15 17:54:29 +0530

    fixed showalerts

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.listen(port, () => {
-    console.log(`Server is Listening on the ${port}...`);
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
