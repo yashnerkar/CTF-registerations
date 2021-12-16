d7db1ab49664c713c9bfd690926fc7648331202b
yash-png <65919365+yash-png@users.noreply.github.com>
2021-12-16 13:53:18 +0530

    update admin.js

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 8000, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
